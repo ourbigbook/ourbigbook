0a63b0c5d5537d48c80c37cd1d2a4e5df0a68853
Ciro Santilli <ciro.santilli@gmail.com>
2021-05-13 01:00:00 +0000

    add a proper not found

--- web/frontend/src/components/Home/MainView.js
+++ web/frontend/src/components/Home/MainView.js
@@ -10,7 +10,6 @@ const YourFeedTab = props => {
       ev.preventDefault();
       props.onTabClick('feed', agent.Articles.feed, agent.Articles.feed());
     }
-
     return (
       <li className="nav-item">
         <a  href=""
@@ -41,20 +40,6 @@ const GlobalFeedTab = props => {
   );
 };
 
-const TagFilterTab = props => {
-  if (!props.tag) {
-    return null;
-  }
-
-  return (
-    <li className="nav-item">
-      <a href="" className="nav-link active">
-        <i className="ion-pound"></i> {props.tag}
-      </a>
-    </li>
-  );
-};
-
 const mapStateToProps = state => ({
   ...state.articleList,
   tags: state.home.tags,
@@ -66,31 +51,23 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const MainView = props => {
-  return (
-    <div className="col-md-9">
-      <div className="feed-toggle">
-        <ul className="nav nav-pills outline-active">
-
-          <YourFeedTab
-            token={props.token}
-            tab={props.tab}
-            onTabClick={props.onTabClick} />
-
-          <GlobalFeedTab tab={props.tab} onTabClick={props.onTabClick} />
-
-          <TagFilterTab tag={props.tag} />
-
-        </ul>
-      </div>
-
-      <ArticleList
-        pager={props.pager}
-        articles={props.articles}
-        loading={props.loading}
-        articlesCount={props.articlesCount}
-        currentPage={props.currentPage} />
-    </div>
-  );
+  return ([
+    <div className="feed-toggle">
+      <ul className="nav nav-pills outline-active">
+        <YourFeedTab
+          token={props.token}
+          tab={props.tab}
+          onTabClick={props.onTabClick} />
+        <GlobalFeedTab tab={props.tab} onTabClick={props.onTabClick} />
+      </ul>
+    </div>,
+    <ArticleList
+      pager={props.pager}
+      articles={props.articles}
+      loading={props.loading}
+      articlesCount={props.articlesCount}
+      currentPage={props.currentPage} />,
+  ]);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainView);

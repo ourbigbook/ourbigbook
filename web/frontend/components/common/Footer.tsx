de4367eb083f598e80415d6ecaa823c26b551b8a
Ciro Santilli <ciro.santilli@gmail.com>
2021-05-13 03:00:02 +0000

    move to next.js
    start to pull the UI appart

---
+++ web/frontend/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+import * as styles from "../../styles";
+
+const Footer = () => (
+  <footer>
+    All website content licensed <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>.
+  </footer>
+);
+
+export default Footer;

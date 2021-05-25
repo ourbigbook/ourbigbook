f51d30d16ca59ad09a74dcb18d12af6e34b51ca6
Ciro Santilli <ciro.santilli@gmail.com>
2021-05-25 02:00:01 +0000

    Typescript working on server it seems

--- web/lib/article.ts
+++ web/lib/article.ts
@@ -1,7 +1,39 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
-import sequelize from "lib/db";
+import sequelize from "./db";
 
-const article = require('./articlejs')
+//const article = require('./articlejs')
+
+export async function getArticle(sequelize, pid) {
+  return await sequelize.models.Article.findOne({
+    where: { slug: pid },
+  });
+}
+
+export async function getArticleWithAuthor(sequelize, pid) {
+  return await sequelize.models.Article.findOne({
+    where: { slug: pid },
+    include: [{ model: sequelize.models.User, as: 'Author' }],
+  });
+}
+
+export async function getArticleJson(sequelize, pid, uid?) {
+  const promises = [
+    getArticleWithAuthor(sequelize, pid)
+  ];
+  if (uid) {
+    promises.push(sequelize.models.User.findByPk(uid))
+  }
+  const ret = await Promise.all(promises);
+  let user;
+  const article = ret[0]
+  if (!article) {
+    return null;
+  }
+  if (uid) {
+    user = ret[1]
+  }
+  return article.toJSONFor(article.Author, user);
+}
 
 export const getStaticPathsArticle: GetStaticPaths = async () => {
   return {
@@ -21,7 +53,7 @@ export const getStaticPathsArticle: GetStaticPaths = async () => {
 export const getStaticPropsArticle: GetStaticProps = async ({ params: { pid } }) => {
   return {
     props: {
-      article: await article.getArticleJson(sequelize, pid)
+      article: await getArticleJson(sequelize, pid)
     }
   };
 }

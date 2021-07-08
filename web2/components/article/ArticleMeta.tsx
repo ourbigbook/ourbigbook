604df0531815f42188ba27372e53c0635beb7b4a
Ciro Santilli <ciro.santilli@gmail.com>
2021-07-08 19:00:03 +0100

    Make article actions not make eyes bleed

--- web2/components/article/ArticleMeta.tsx
+++ web2/components/article/ArticleMeta.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ArticleActions from "components/article/ArticleActions";
 import CustomLink from "components/common/CustomLink";
 import UserLinkWithImage from "components/common/UserLinkWithImage";
-import FollowUserButton from "components/profile/FollowUserButton";
 import { formatDate } from "lib/utils/date";
 
 const ArticleMeta = ({ article }) => {
@@ -13,16 +12,14 @@ const ArticleMeta = ({ article }) => {
   return (
     <div className="article-meta">
       <UserLinkWithImage user={article.author} />
-      <FollowUserButton profile={article.author} />
-      <div className="info">
-        Created: <span className="date">{formatDate(article.createdAt)}</span>
-        {article.createdAt !== article.updatedAt &&
-          <>
-            {' '}
-            Updated: <span className="date">{formatDate(article.updatedAt)}</span>
-          </>
-        }
-      </div>
+      {' '}
+      {formatDate(article.createdAt)}
+      {article.createdAt !== article.updatedAt &&
+        <>
+          {' '}
+          Updated: {formatDate(article.updatedAt)}
+        </>
+      }
       <ArticleActions article={article} />
     </div>
   );

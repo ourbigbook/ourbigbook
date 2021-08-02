87b38475fd9645612068c8ac95c220b02a1762ff
Ciro Santilli <ciro.santilli@gmail.com>
2021-08-02 20:58:30 +0100

    web: add logout buttong to user page for the love

--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -1,7 +1,8 @@
 import Router from "next/router";
 import React from "react";
-import { mutate, trigger } from "swr";
+import { trigger } from "swr";
 
+import LogoutButton from "components/common/LogoutButton";
 import SettingsForm from "components/profile/SettingsForm";
 import checkLogin from "lib/utils/checkLogin";
 import storage from "lib/utils/storage";
@@ -15,16 +16,10 @@ const Settings = () => {
       }
     })
   })
-  const handleLogout = async (e) => {
-    e.preventDefault();
-    window.localStorage.removeItem("user");
-    mutate("user", null);
-    Router.push(`/`).then(() => trigger("user"));
-  };
   return (
     <div className="settings-page content-not-cirodown">
       <h1>Your Settings</h1>
-      <button className="btn" onClick={handleLogout}>Logout</button>
+      <LogoutButton />
       <SettingsForm />
     </div>
   );

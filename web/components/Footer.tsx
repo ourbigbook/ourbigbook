a9ceaf448cbfb63a88e6b90cf4120f663c8e1cc5
Ciro Santilli <ciro.santilli@gmail.com>
2021-11-12 20:13:46 +0000

    contact

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <div className="container">
       Content license <a href="https://cirosantilli.com/ourbigbook-com/content-license">CC BY-SA 4.0 unless noted</a>  |
       {' '}<a href="https://github.com/cirosantilli/cirodown/tree/master/web">Website source code</a> |
-      {' '}<a href="https://github.com/cirosantilli/cirodown/issues">Contat, bugs, suggestions, abuse reports</a>
+      {' '}<a href="https://github.com/cirosantilli/cirodown/issues">Contact, bugs, suggestions, abuse reports</a>
     </div>
   </footer>
 );

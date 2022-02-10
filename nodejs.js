df8d6ca2f5a5e0f408c446016a269ab115b1c9c0
Ciro Santilli <ciro.santilli@gmail.com>
2022-02-10 22:00:59 +0000

    refactor: rename dag and graph to tree

--- nodejs.js
+++ nodejs.js
@@ -223,7 +223,7 @@ class SqliteIdProvider extends cirodown.IdProvider {
 
   // We have a separate function from fetch_header_tree_ids to defer after that call,
   // because we want to first fetch everything
-  // and populate the ID cache with the include entry points that have proper header_dag_node.
+  // and populate the ID cache with the include entry points that have proper header_tree_node.
   // Only then are we ready for linking up the rest of the tree.
   build_header_tree(starting_ids_to_asts, fetch_header_tree_ids_rows, { context }) {
     const asts = []
@@ -231,12 +231,12 @@ class SqliteIdProvider extends cirodown.IdProvider {
       const ast = this.row_to_ast(row, context)
       const parent_id = row.from_id
       const parent_ast = this.id_cache[parent_id]
-      const parent_ast_header_dag_node = parent_ast.header_dag_node
-      ast.header_dag_node = new cirodown.HeaderDagNode(ast, parent_ast_header_dag_node);
+      const parent_ast_header_tree_node = parent_ast.header_tree_node
+      ast.header_tree_node = new cirodown.HeaderTreeNode(ast, parent_ast_header_tree_node);
       // I love it when you get potential features like this for free.
       // Only noticed when Figures showed up on ToC.
       if (ast.macro_name === cirodown.Macro.HEADER_MACRO_NAME) {
-        parent_ast_header_dag_node.add_child(ast.header_dag_node);
+        parent_ast_header_tree_node.add_child(ast.header_tree_node);
       }
       cirodown.propagate_numbered(ast, context)
       this.id_cache[ast.id] = ast

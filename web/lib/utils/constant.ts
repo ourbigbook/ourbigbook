478a6feb85c7a957b172072a95f0a5beff7ba403
Ciro Santilli <ciro.santilli@gmail.com>
2022-03-01 09:16:55 +0000

    web: remove unused tag table
    This was a remnant from the RealWorld tech baseline website.
    
    We will never use this here, since for use tags are articles, and
    parent relationships are already clearly encoded in the Refs table.

--- web/lib/utils/constant.ts
+++ web/lib/utils/constant.ts
@@ -5,7 +5,6 @@ export const ABOUT_HREF = "https://cirosantilli.com/ourbigbook-com"
 export const APP_NAME = `OurBigBook.com`;
 export const ARTICLE_QUERY_MAP = {
   "tab=feed": `${SERVER_BASE_URL}/articles/feed`,
-  "tab=tag": `${SERVER_BASE_URL}/articles/tag`
 };
 export const BUTTON_ACTIVE_CLASS = 'active';
 export const DEFAULT_PROFILE_IMAGE = `https://static.productionready.io/images/smiley-cyrus.jpg`;

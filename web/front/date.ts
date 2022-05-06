78844fed1bd527039643ac71c16b4de78e1b85c5
Ciro Santilli <ciro.santilli@gmail.com>
2022-05-06 08:42:16 +0100

    web: fix month display, was 0 based
    Fix: https://github.com/cirosantilli/ourbigbook/issues/233

--- web/front/date.ts
+++ web/front/date.ts
@@ -2,5 +2,5 @@ const zeroPad = (num, places) => String(num).padStart(places, '0')
 
 export function formatDate(dateString) {
   const date = new Date(dateString)
-  return `${date.getFullYear()}-${zeroPad(date.getMonth(), 2)}-${zeroPad(date.getDate(), 2)}`
+  return `${date.getFullYear()}-${zeroPad(date.getMonth() + 1, 2)}-${zeroPad(date.getDate(), 2)}`
 }

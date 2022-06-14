ourbigbook/ourbigbook

commit 08b7980fdc24bda7a628aac5019d5e830ef4ff85

lib: --format-output fix empty arguments/arguments ending in list followed by paragraph
Horrendous.

index.js
@@ -8770,16 +8770,7 @@ function ourbigbook_add_newlines_after_block(ast, context) {
       )
     )
   ) {
-    let n = 2
-    if (context.last_render) {
-      if (context.last_render[context.last_render.length - 1] === '\n') {
-        n--
-        if (context.last_render[context.last_render.length - 2] === '\n') {
-          n--
-        }
-      }
-    }
-    return n
+    return 2
   } else {
     return 0
   }
@@ -8873,7 +8864,10 @@ function ourbigbook_convert_args(ast, context, options={}) {
       ret_arg.push(rendered_arg)
       if (
         has_newline &&
-        rendered_arg[rendered_arg.length - 1] !== '\n'
+        (
+          rendered_arg.length === 0 ||
+          rendered_arg[rendered_arg.length - 1] !== '\n'
+        )
       ) {
         ret_arg.push('\n')
       }
@@ -8923,7 +8917,10 @@ function ourbigbook_convert_args(ast, context, options={}) {
         ret_arg.push(rendered_arg)
         if (
           has_newline &&
-          rendered_arg[rendered_arg.length - 1] !== '\n'
+          (
+            rendered_arg.length === 0 ||
+            rendered_arg[rendered_arg.length - 1] !== '\n'
+          )
         ) {
           ret_arg.push('\n')
         }

test_bigb_output.bigb
@@ -75,6 +75,20 @@ asdf\br
 qwer
 ]
 
+Empty argument followed by paragraph:
+
+\Q[]
+
+and something after.
+
+Argument ending in list:
+
+\Q[
+* asdf
+]
+
+and something after.
+
 Non-br macro without arguments\i
 followed by a newline
 

commit ecce33e16455b3dc0747fe24eba6c080febeca98

lib: --format-source: refix bigb output: nested sane list followed by paragraph
Was broken after 08b7980fdc24bda7a628aac5019d5e830ef4ff85 tried to fix
something else.

index.js
@@ -8753,7 +8753,8 @@ function ourbigbook_li(marker) {
   }
 }
 
-function ourbigbook_add_newlines_after_block(ast, context) {
+function ourbigbook_add_newlines_after_block(ast, context, options={}) {
+  const { auto_parent } = options
   if (
     !context.macros[ast.macro_name].options.phrasing &&
     !ast.is_last_in_argument() &&
@@ -8770,25 +8771,25 @@ function ourbigbook_add_newlines_after_block(ast, context) {
           ast.parent_ast.parent_ast.macro_name === Macro.TOPLEVEL_MACRO_NAME
         ) &&
         ast.parent_ast.macro_name !== Macro.TOPLEVEL_MACRO_NAME &&
-          (
-            ( ast.parent_argument.has_paragraph ) ||
-            ( !ast.parent_argument.has_paragraph && !ast.parent_argument.not_all_block )
-          )
+        (
+          ( ast.parent_argument.has_paragraph ) ||
+          ( !ast.parent_argument.has_paragraph && !ast.parent_argument.not_all_block )
+        )
       )
     )
   ) {
-    return 2
-    // TODO
-    //let n = 2
-    //if (context.last_render) {
-    //  if (context.last_render[context.last_render.length - 1] === '\n') {
-    //    n--
-    //    if (context.last_render[context.last_render.length - 2] === '\n') {
-    //      n--
-    //    }
-    //  }
-    //}
-    //return n
+    let n = 2
+    if (auto_parent) {
+      if (context.last_render) {
+        if (context.last_render[context.last_render.length - 1] === '\n') {
+          n--
+          if (context.last_render[context.last_render.length - 2] === '\n') {
+            n--
+          }
+        }
+      }
+    }
+    return n
   } else {
     return 0
   }
@@ -8799,7 +8800,7 @@ function ourbigbook_ul(ast, context) {
     return ourbigbook_convert_simple_elem(ast, context)
   } else {
     const argstr = render_arg(ast.args.content, context)
-    const newline = '\n'.repeat(ourbigbook_add_newlines_after_block(ast, context))
+    const newline = '\n'.repeat(ourbigbook_add_newlines_after_block(ast, context, { auto_parent: true }))
     return `${argstr}${newline}`
   }
 }

test.js
@@ -6352,12 +6352,12 @@ Named multiline second
   },
 );
 assert_convert_ast('bigb output: nested sane list followed by paragraph',
-  // TODO this was leading to an AST change because the input has inner list as
-  // `ccc\n` but the output only `ccc`. But lazy to fix now, maybe what we want is the
-  // input to parse as `ccc` without the `\n`.
+  // This was leading to an AST change because the input has inner list as
+  // `ccc\n` but the output only `ccc`. But lazy to fix now, what we want is the
+  // input to parse as `ccc` without the `\n`: https://github.com/cirosantilli/ourbigbook/issues/245
   `aaa
 
-* bb
+* bbb
   \\Ul[
   * ccc
   ]
@@ -6368,7 +6368,7 @@ ddd
   {
     assert_bigb_main: `aaa
 
-* bb
+* bbb
   * ccc
 
 ddd
bc100a16a75294efcbc2ed2a8b1da1eb0d929ad6
Ciro Santilli <ciro.santilli@gmail.com>
2022-07-12 03:00:02 +0000

    web: redirect user to register page from article editor if not logged in
    We do have some links that show up to new article when the user is not
    logged in.
    
    Those are left in places where we don't have enough space to ask "Sign up
    or sign in" in full. But we like to keep those "Create" links to make it
    more likely that people will sign up.
    
    If I break Navbar display again, this commit is the culprit.

--- web/front/useLoggedInUser.ts
+++ web/front/useLoggedInUser.ts
@@ -1,13 +1,15 @@
 import useSWR from 'swr'
-import React from 'react'
 
 import checkLogin from 'front/checkLogin'
 import storage from 'front/storage'
 import { AUTH_COOKIE_NAME, AUTH_LOCAL_STORAGE_NAME, getCookie } from 'front'
 
+// @return * undefined: don't know yet, waiting to access local memory asynchronously
+//         * null: checked and we are definitely not logged in.
 export default function useLoggedInUser() {
-    React.useEffect(() => {})
-    const { data: authCookie } = useSWR(AUTH_COOKIE_NAME, () => {
+  const { data: authCookie } = useSWR(
+    AUTH_COOKIE_NAME,
+    () => {
       const ret = getCookie(AUTH_COOKIE_NAME)
       if (!ret) {
         // E.g. if the test database was nuked, the GET request sees wrong auth,
@@ -15,15 +17,30 @@ export default function useLoggedInUser() {
         // the JavaSript, so we get rid of it. Notably, this removes the logged in
         // user from the navbar.
         window.localStorage.removeItem(AUTH_LOCAL_STORAGE_NAME);
+        return null
       }
       return ret
-    })
-    const { data: loggedInUser } = useSWR(() => authCookie ? AUTH_LOCAL_STORAGE_NAME : null, storage);
-    if (loggedInUser === undefined) return loggedInUser
-    const isLoggedIn = checkLogin(loggedInUser);
-    if (isLoggedIn) {
-      return loggedInUser;
-    } else {
-      return null;
     }
+  )
+  const { data: loggedInUser } = useSWR(
+    () => authCookie ? AUTH_LOCAL_STORAGE_NAME : null,
+    () => {
+      const ret = storage(AUTH_LOCAL_STORAGE_NAME)
+      if (ret === undefined) {
+        return null
+      } else {
+        return ret
+      }
+    }
+  );
+  if (authCookie === null) {
+    return null;
+  }
+  if (loggedInUser === undefined) return loggedInUser
+  const isLoggedIn = checkLogin(loggedInUser);
+  if (isLoggedIn) {
+    return loggedInUser;
+  } else {
+    return null;
+  }
 }

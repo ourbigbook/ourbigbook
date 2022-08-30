e324421ce3f21faf250673e3c5365004a7b0cfff
OurBigBook Admin <admin@ourbigbook.com>
2022-08-30 07:07:33 +0000

    vscode: create extension with syntax highlighting only

---
+++ vscode/README.md
@@ -0,0 +1,7 @@
+# OurBigBook VS Code Extension
+
+This extension adds support for local [OurBigBook Markup](https://docs.ourbigbook.com/ourbigbook-markup) and [OurBigBook Web](https://docs.ourbigbook.com/ourbigbook-web) and [OurBigBook CLI](https://docs.ourbigbook.com/ourbigbook-cli) deployment to Visual Studio Code.
+
+This extension's documentation can be found at: [OurBigBook Markup](https://docs.ourbigbook.com/visual-studio-code).
+
+* asdf
---
+++ vscode/extension.js
@@ -0,0 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	console.log('bigb.activate');
+	let disposable = vscode.commands.registerCommand('bigb.helloWorld', function () {
+		vscode.window.showInformationMessage('Hello World from asdf!');
+		console.log('hello world command');
+	});
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}

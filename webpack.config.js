a534e52e1d4bc2eca22fa2d0b47996c6218ab00f
Ciro Santilli <cirosantilli@proton.me>
2024-06-13 12:10:03 +0100

    editor: make editor.html actually work this time!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
-const path = require('path');
+const path = require('path')
 
-const ourbigbook_nodejs_webpack_safe = require('./nodejs_webpack_safe.js');
+const ourbigbook_nodejs_webpack_safe = require('./nodejs_webpack_safe.js')
 
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+/* Currently this is used only for copying editor.html into dist/
+ * This was the simplest way we could find to get editor.html to just work both locally
+ * and on publish, as then we can just use relative paths inside _dist and all works well.
+ *
+ * We also then minify editor.html just for fun.
+ *
+ * Ideally however, we would like to pass this through HtmlWebpackPlugin and minify it there.
+ *
+ * If we ut editor.html as an entry, webpack does pick up everything nicely by input filename,
+ * e.g. we can use stuff like:
+ *
+ *     <link rel="stylesheet" href="ourbigbook.scss">
+ *
+ * The problem is that this outputs .js rather than .html, and the Internet hasn't figured out yet how:
+ *
+ * * https://stackoverflow.com/questions/45063362/using-webpack-with-a-html-entry
+ * * https://stackoverflow.com/questions/55082181/html-as-webpack-entry-point
+ */
+const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin")
 
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules')
 
 module.exports = {
   entry: {
     ourbigbook: ['./index.js'],
     ourbigbook_runtime: ['./ourbigbook_runtime.js'],
     ourbigbook_css: ['./ourbigbook.scss'],
-    editor: ['./editor.scss'],
+    editor: ['./editor.js'],
+    editor_css: ['./editor.scss'],
   },
   mode: 'production',
   module: {
@@ -62,12 +83,18 @@ module.exports = {
       },
     }),
     new NodePolyfillPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'editor.html' },
+      ],
+    }),
   ],
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
       // Minimizes the JavaScript.
       new TerserPlugin(),
+      new HtmlMinimizerPlugin(),
     ],
     minimize: true,
   },

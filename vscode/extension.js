8fc2ebe4c80b564e6fbc213d3b0ac7d6c55f3017
Ciro Santilli <cirosantilli@proton.me>
2024-07-24 22:33:18 +0100

    vscode: release with debug

--- vscode/extension.js
+++ vscode/extension.js
@@ -12,62 +12,12 @@ const MAX_IDS = 10000
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-class OurbigbookWorkspaceSymbolProvider {
-	/** The query only contains the string before the first space typed into
-	 * the Ctrl+T bar... Related:
-	 * https://github.com/microsoft/vscode/issues/93645
-	 * Anything after the first space is used by vscode as a further
-	 * filter over something, not excactly symbol names either, so it is quite sad.
-	 * We would need to implement our own custom search window to overcome this.
-	 */
-	async provideWorkspaceSymbols(query, token) {
-		const workspaceFolders = vscode.workspace.workspaceFolders
-		let curdir
-		if (workspaceFolders) {
-			curdir = workspaceFolders[0]
-		} else {
-			curdir = path.dirname(vscode.window.activeTextEditor.document.fileName)
-		}
-		const ourbigbookJsonDir = ourbigbook_nodejs_webpack_safe.findOurbigbookJsonDir(curdir)
-		if (ourbigbookJsonDir) {
-			const sequelize = await ourbigbook_nodejs_webpack_safe.createSequelize({
-				logging: false,
-				storage: path.join(ourbigbookJsonDir, ourbigbook_nodejs_webpack_safe.TMP_DIRNAME, ourbigbook_nodejs_front.SQLITE_DB_BASENAME),
-			})
-			return Promise.all([
-				sequelize.models.Id.findAll({
-					where: { idid: { [sequelize.Sequelize.Op.startsWith]: query } },
-					order: [['idid', 'ASC']],
-					limit: MAX_IDS,
-				}),
-				sequelize.models.Id.findAll({
-					where: { idid: { [sequelize.Sequelize.Op.substring]: query } },
-					order: [['idid', 'ASC']],
-					limit: MAX_IDS,
-				}),
-			]).then(ids => ids.flat().map(id => {
-				const json = JSON.parse(id.ast_json)
-				const sourceLocation = json.source_location
-				return new vscode.SymbolInformation(
-					id.idid,
-					vscode.SymbolKind.Variable,
-					undefined,
-					new vscode.Location(
-						vscode.Uri.file(path.join(ourbigbookJsonDir, sourceLocation.path)),
-						new vscode.Position(sourceLocation.line - 1, sourceLocation.column - 1),
-					)
-				)
-			}))
-		}
-	}
-}
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
 	const channel = vscode.window.createOutputChannel('OurBigBook', 'ourbigbook')
-	channel.appendLine('ourbigbook.activate OutputChannel')
+	channel.appendLine('ourbigbook.activate OutputChannel.appendLine')
 	console.log('ourbigbook.activate log')
 	let helloWorld = vscode.commands.registerCommand('ourbigbook.helloWorld', async function () {
 		console.log('ourbigbook.helloWorld console.log')
@@ -75,6 +25,59 @@ async function activate(context) {
 		vscode.window.showInformationMessage('Hello World from OurBigBook!')
 	})
 	context.subscriptions.push(helloWorld)
+
+	class OurbigbookWorkspaceSymbolProvider {
+		/** The query only contains the string before the first space typed into
+		 * the Ctrl+T bar... Related:
+		 * https://github.com/microsoft/vscode/issues/93645
+		 * Anything after the first space is used by vscode as a further
+		 * filter over something, not exactly symbol names either, so it is quite sad.
+		 * We would need to implement our own custom search window to overcome this.
+		 */
+		async provideWorkspaceSymbols(query, token) {
+			channel.appendLine(`provideWorkspaceSymbols query=${query}`)
+			const workspaceFolders = vscode.workspace.workspaceFolders
+			let curdir
+			if (workspaceFolders) {
+				curdir = workspaceFolders[0]
+			} else {
+				curdir = path.dirname(vscode.window.activeTextEditor.document.fileName)
+			}
+			channel.appendLine(`provideWorkspaceSymbols curdir=${curdir}`)
+			const ourbigbookJsonDir = ourbigbook_nodejs_webpack_safe.findOurbigbookJsonDir(curdir)
+			channel.appendLine(`provideWorkspaceSymbols ourbigbookJsonDir=${ourbigbookJsonDir}`)
+			if (ourbigbookJsonDir) {
+				const sequelize = await ourbigbook_nodejs_webpack_safe.createSequelize({
+					logging: (s) => channel.appendLine(`provideWorkspaceSymbols sql=${s}`),
+					storage: path.join(ourbigbookJsonDir, ourbigbook_nodejs_webpack_safe.TMP_DIRNAME, ourbigbook_nodejs_front.SQLITE_DB_BASENAME),
+				})
+				return Promise.all([
+					sequelize.models.Id.findAll({
+						where: { idid: { [sequelize.Sequelize.Op.startsWith]: query } },
+						order: [['idid', 'ASC']],
+						limit: MAX_IDS,
+					}),
+					sequelize.models.Id.findAll({
+						where: { idid: { [sequelize.Sequelize.Op.substring]: query } },
+						order: [['idid', 'ASC']],
+						limit: MAX_IDS,
+					}),
+				]).then(ids => ids.flat().map(id => {
+					const json = JSON.parse(id.ast_json)
+					const sourceLocation = json.source_location
+					return new vscode.SymbolInformation(
+						id.idid,
+						vscode.SymbolKind.Variable,
+						undefined,
+						new vscode.Location(
+							vscode.Uri.file(path.join(ourbigbookJsonDir, sourceLocation.path)),
+							new vscode.Position(sourceLocation.line - 1, sourceLocation.column - 1),
+						)
+					)
+				}))
+			}
+		}
+	}
 	context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(new OurbigbookWorkspaceSymbolProvider()))
 }
 

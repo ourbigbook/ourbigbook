ourbigbook/ourbigbook

commit b8fb10ef58fd5247736ed70cb958e6fa14226d14

web: show article as announced immediately on production build
Dev build was updating but only because it does extra updates like mad.
This framework is insane, dev mode does not reflect production mode.

web/front/Article.tsx
@@ -107,6 +107,7 @@ function LinkListNoTitle({
 function AnnounceModal({
   article,
   router,
+  setArticle,
   setShowAnnounce,
 }) {
   const [announceMessage, setAnnounceMessage] = React.useState('')
@@ -137,8 +138,9 @@ function AnnounceModal({
         }}
       >
       </textarea>
-      <button onClick={() => {
-        webApi.articleAnnounce(article.slug, announceMessage)
+      <button onClick={async () => {
+        const ret = await webApi.articleAnnounce(article.slug, announceMessage)
+        setArticle(ret.data.article)
         setShowAnnounce(false)
         Router.push(removeParameterFromUrlPath(router.asPath, ANNOUNCE_QUERY_PARAM), undefined, { scroll: false })
       }}>
@@ -373,7 +375,7 @@ function WebMeta({
 /** The name of this element is not very accurate, it should likely be ArticleDescendantsAndMeta or something like that. */
 export default function Article({
   ancestors,
-  article,
+  article: articleInit,
   articlesInSamePage,
   articlesInSamePageCount,
   articlesInSamePageForToc,
@@ -394,6 +396,10 @@ export default function Article({
   topIssues,
 }) {
   let t0
+  // Initially putting this under state for the announce to articles modal to show "announced"
+  // as soon as you finish announcing. In general we need to use this pattern whenever the data
+  // is modified and we want to show an update to user immediately on the same page.
+  const [article, setArticle] = React.useState(articleInit)
   const authorUsername = article.author.username
   const [curComments, setComments] = React.useState(comments)
   const [curCommentsCount, setCommentsCount] = React.useState(commentsCount)
@@ -405,10 +411,11 @@ export default function Article({
   const [showNewListener, setShowNewListener] = React.useState(undefined)
   const getParamString = encodeGetParams(router.query)
   React.useEffect(() => {
+    setArticle(articleInit)
     // Otherwise comments don't change on page changes.
     setComments(comments)
     setCommentsCount(commentsCount)
-  }, [getParamString, comments, commentsCount])
+  }, [getParamString, articleInit, comments, commentsCount])
   React.useEffect(() => {
     setShowNew(queryNew)
   }, [queryNew])
@@ -1061,6 +1068,7 @@ export default function Article({
     {showAnnounce && <AnnounceModal {...{
       article,
       router,
+      setArticle,
       setShowAnnounce,
     }} />}
     <div

commit 7ded6b9b5ca2a83cc58178cf7c2cd7c885b9e00a

web: fix blowup intruduced with article state due to negative levels
The issue was introduced in b8fb10ef58fd5247736ed70cb958e6fa14226d14
where article was made into a state. Then when moving pages, article would
be outdated, but the articlesInSamePageForToc prop would be up to date,
leading to a blowup.

web/front/Article.tsx
@@ -411,7 +411,16 @@ export default function Article({
   // Initially putting this under state for the announce to articles modal to show "announced"
   // as soon as you finish announcing. In general we need to use this pattern whenever the data
   // is modified and we want to show an update to user immediately on the same page.
-  const [article, setArticle] = React.useState(articleInit)
+  let [article, setArticle] = React.useState(articleInit)
+  if (articleInit.id !== article.id) {
+    // We moved between articles, and haven't updated yet.
+    // articlesInSamePageForToc could be out-of-sync with article
+    // which led to a negative level blowup in renderTocFromEntryList
+    // when moving e.g.
+    // - from: /pioyi/ion-selective-electrodes/response-of-the-glass-electrode
+    // - to: /pioyi/ion-selective-electrodes
+    article = articleInit
+  }
   const authorUsername = article.author.username
   const [curComments, setComments] = React.useState(comments)
   const [curCommentsCount, setCommentsCount] = React.useState(commentsCount)
@@ -423,8 +432,9 @@ export default function Article({
   const [showNewListener, setShowNewListener] = React.useState(undefined)
   const getParamString = encodeGetParams(router.query)
   React.useEffect(() => {
+    // Otherwise these don't change on page changes.
+    // https://stackoverflow.com/questions/63143334/how-to-not-persist-state-between-next-js-dynamic-routes
     setArticle(articleInit)
-    // Otherwise comments don't change on page changes.
     setComments(comments)
     setCommentsCount(commentsCount)
   }, [getParamString, articleInit, comments, commentsCount])